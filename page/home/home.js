177ccb55984cb68840e051c7a11dff9e5a972a2e
oldpower <851903287@qq.com>
2020-01-19 13:32:11 +0800

    小程序初体验

--- page/home/home.js
+++ page/home/home.js
@@ -5,8 +5,34 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    name:'date bangding',
+    students: [
+      {id: 1001, name: 'a', age: 30},
+      {id: 1002, name: 'b', age: 31 },
+      {id: 1003, name: 'c', age: 32 }
+    ],
+    counter:0
   },
+
+
+  handleBtnClick(){
+    //1.这种方式,界面不会刷新
+    // this.data.count += 1
+    // console.log(this.data.count)
+
+    //2.this.setData()
+    this.setData({
+      counter: this.data.counter + 1
+    })
+    //console.log('clices 100')
+  },
+  handleSubterClick(){
+    this.setData({
+      counter: this.data.counter - 1
+    })
+  },
+
+
 
   /**
    * 生命周期函数--监听页面加载
